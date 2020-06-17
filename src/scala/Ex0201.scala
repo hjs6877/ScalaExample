3daa41fb2555e7545ab7067ab2d6f7f728d56c7a
jshwang <jshwang@pentasecurity.com>
2020-06-17 10:49:21 +0900

    Initial commit

---
+++ src/scala/Ex0201.scala
@@ -0,0 +1,11 @@
+package scala
+
+object Ex0201 {
+  def main(args: Array[String]): Unit = {
+    var a = "변수이다"
+    val b = "final 변수이다"
+    a = "바꿀 수 있음"
+//    b = "바꿀 수 없음"
+    println(a)
+  }
+}
